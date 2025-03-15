5c518eb4d4bdaa2aa028147a9e92a31c7de8e92a
Nicolas10Montex <montexdev@protonmail.com>
2025-03-15 11:23:09 +0100

    Program installation

--- .auto/apt.sh
+++ .auto/apt.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
-sudo apt install zsh git  gh stow gcc python3 curl fd-find eza rgrep
+# Some utils
+sudo apt install zsh gh stow gcc python3 curl fd-find eza rgrep wget tldr -y
 
+# Some programs
+sudo apt install qbittorrent -y
+
+# NVM 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-echo "Run 'nvm install node'"
-
 sudo apt update && sudo apt install extrepo -y
 
+# Librewolf
 sudo extrepo enable librewolf
-
 sudo apt update && sudo apt install librewolf -y
 
+# Rclone 
+wget https://downloads.rclone.org/v1.69.1/rclone-v1.69.1-linux-amd64.deb
+sudo apt dpkg -i ./rclone-v1.69.1-linux-amd-64.deb
+rm -rf rclone-v1.69.1-linux-amd-64.deb
+
+sudo apt-get -y install podman
+
+# Signal
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
+
+# Steam
+wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
+sudo apt install ./steam.deb -y
+rm -rf steam.deb
+
+# Lutris
+wget https://github.com/lutris/lutris/releases/download/v0.5.18/lutris_0.5.18_all.deb
+sudo apt install ./lutris_0.5.18_all.deb -y
+rm -rf lutris_0.5.18_all.deb 
+
+sudo apt autoremove
+
+echo "Run 'nvm install node'"
--- .auto/flatpak.sh
+++ .auto/flatpak.sh
@@ -3,5 +3,11 @@
 sudo apt install flatpak
 sudo apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
 echo "Make sure to reboot to change the update the system"
+
+flatpak install -y md.obsidian.Obsidian
+flatpak install -y org.telegram.desktop
+flatpak install -y io.missioncenter.MissionCenter
+flatpak install -y org.jellyfin.JellyfinServer
+flatpak install -y app.zen_browser.zen
+
