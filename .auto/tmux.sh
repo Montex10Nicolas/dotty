1afb005389bd7030e9e03a5d87c51edb75e49dbb
Nicolas10Montex <montexdev@protonmail.com>
2025-03-18 18:40:52 +0100

    tmux themes and tmux install

--- .auto/tmux.sh
+++ .auto/tmux.sh
@@ -5,12 +5,4 @@ if ! command -v tmux 2>&1 > /dev/null; then
   exit
 fi
 
-
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# List of plugins
-set -g @plugin 'tmux-plugins/tpm'
-set -g @plugin 'tmux-plugins/tmux-sensible'
-
-# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-echo run '~/.tmux/plugins/tpm/tpm' | tee ~/.tmux.conf
