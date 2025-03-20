306516b3c614e550b40b3aa4ddea7a0586a1bb3d
Nicolas10Montex <montexdev@protonmail.com>
2025-03-20 18:14:31 +0100

    fixed warning

--- nvim/.config/nvim/lua/custom/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/custom/plugins/bufferline.lua
@@ -28,15 +28,15 @@ return {
         },
       },
     },
-    config = function(_, opts)
-      require('bufferline').setup(opts)
-      vim.api.nvim_create_autocmd({ 'BufAdd', 'BufDelete' }, {
-        callback = function()
-          vim.schedule(function()
-            pcall(nvim_bufferline)
-          end)
-        end,
-      })
-    end,
   },
+  config = function(_, opts)
+    require('bufferline').setup(opts)
+    vim.api.nvim_create_autocmd({ 'BufAdd', 'BufDelete' }, {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
+  end,
 }
