10e032fc2cb9ce0d5d2eb609afef9d6aec4f1915
Nicolas10Montex <montexdev@protonmail.com>
2025-03-20 18:09:57 +0100

    bufferline offset

--- nvim/.config/nvim/lua/custom/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/custom/plugins/bufferline.lua
@@ -1,9 +1,42 @@
 return {
   'akinsho/bufferline.nvim',
-  version = '*',
+  event = 'VeryLazy',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    vim.opt.termguicolors = true
-    require('bufferline').setup()
-  end,
+  keys = {
+    { '<leader>pp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
+    { '<leader>p', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
+    { '<leader>pl', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
+    { '<leader>ph', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
+    { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+    { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+    { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+    { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+    { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
+    { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+    { '<leader>C', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+  },
+  opts = {
+    options = {
+      diagnostics = 'nvim_lsp',
+      always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'Neo-tree',
+          highlight = 'Directory',
+          text_align = 'left',
+        },
+      },
+    },
+    config = function(_, opts)
+      require('bufferline').setup(opts)
+      vim.api.nvim_create_autocmd({ 'BufAdd', 'BufDelete' }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
 }
