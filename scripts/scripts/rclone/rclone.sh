8bcb32bcb29055a8a36d95848cad5e0d097cc20c
Nicolas10Montex <montexdev@protonmail.com>
2025-06-10 18:23:40 +0200

    rclone

--- scripts/scripts/rclone/rclone.sh
+++ scripts/scripts/rclone/rclone.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rclone copy -P -L /home/jumong/Obsidian/ mega:Obsidian
 rclone copy /mnt/torrent1/torrent/ mega:torrent
-rclone copy -P /home/jumong/Pictures/ mega:Picture
 rclone copy -P /home/jumong/Documents/ mega:Documents
