2d15aca89d85e70e51fd3f1b9a49a639f6581849
Nicolas10Montex <montexdev@protonmail.com>
2025-11-01 16:55:24 +0100

    local

--- scripts/scripts/rclone/rclone.sh
+++ scripts/scripts/rclone/rclone.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+# MEGA
+echo "Obsidian"
 rclone copy -P -L /home/joyoung/Obsidian/ mega:Obsidian
-rclone copy /mnt/torrent1/torrent/ mega:torrent
+echo "Torrent"
+rclone copy -P -L /mnt/torrent1/torrent/ mega:torrent
+
+# Local
+echo "Kavita"
+rclone copy -P -L -c ~/Kavita/ /mnt/external/RClone/Kavita/
+
+echo "Mangas"
+rclone copy -P -L -c ~/Documents/Mangas/ /mnt/external/RClone/Mangas/
