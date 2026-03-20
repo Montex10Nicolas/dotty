02cd97db90a9939ead2966ddc1376e5101e756de
Nicolas10Montex <montexdev@protonmail.com>
2026-03-20 09:06:14 +0100

    idk

--- scripts/scripts/rclone/rclone.sh
+++ scripts/scripts/rclone/rclone.sh
@@ -5,24 +5,11 @@
 # -L: copy-links follow symlinks
 
 # MEGA
-echo "Obsidian"
+echo "Obsidian Mega"
 rclone copy -P -L /home/joyoung/Obsidian/ mega:Obsidian
-echo "Torrent"
+echo "Torrent Mega"
 rclone copy -P -L /mnt/torrent1/torrent/ mega:torrent
 
-# Local
-echo "Kavita"
-rclone copy -P -L -c ~/Kavita/ /mnt/external/RClone/Kavita/
-printf "Kavita done\n\n"
-
-echo "Prisma instances"
-rclone copy -P -L -c ~/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/ /mnt/external/RClone/MinecraftTFC
-printf "Prisma done\n\n"
-
-echo "Mangas"
-rclone copy -P -L -c ~/Documents/Mangas/ /mnt/external/RClone/Mangas/
-rclone copy -P -L -c ~/Documents/MangaRead/ /mnt/external/RClone/Mangas/
-printf "Mangas done\n\n"
-
 # Watcharr
+echo "Watcharr Mega"
 rclone copy -P -L -c ~/Watcharr mega:Watcharr
